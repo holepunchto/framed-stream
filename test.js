3eae43e25d57d4bf44a8ff3aad41649729fa5b96
Lucas Barrena <lucasbarrena98@gmail.com>
2022-12-06 14:49:39 -0300

    more tests

--- test.js
+++ test.js
@@ -3,16 +3,16 @@ const FramedStream = require('./index.js')
 const duplexThrough = require('duplex-through')
 const b4a = require('b4a')
 
-test('basic', function (t) {
+test('full cycle', function (t) {
   t.plan(8)
 
   const [a, b] = create()
 
   a.rawStream.once('data', function (raw) {
-    t.alike(raw, b4a.concat([b4a.from([5,0,0,0]), b4a.from('hello')]), 'a first raw data')
-    
+    t.alike(raw, b4a.concat([b4a.from([5, 0, 0, 0]), b4a.from('hello')]), 'a first raw data')
+
     a.rawStream.once('data', function (raw) {
-      t.alike(raw, b4a.concat([b4a.from([6,0,0,0]), b4a.from('world!')]), 'a second raw data')
+      t.alike(raw, b4a.concat([b4a.from([6, 0, 0, 0]), b4a.from('world!')]), 'a second raw data')
 
       a.rawStream.once('data', function () {
         t.fail('a should not receive more raw data')
@@ -34,13 +34,13 @@ test('basic', function (t) {
         t.fail('a should not receive more messages')
       })
 
-      // a.end()
+      a.end()
     })
   })
 
   a.on('end', function () {
     t.pass('a end')
-    a.end()
+    // a.end()
   })
 
   a.on('close', function () {
@@ -53,7 +53,7 @@ test('basic', function (t) {
 
   b.on('end', function () {
     t.pass('b end')
-    // b.end()
+    b.end()
   })
 
   b.on('close', function () {
@@ -62,16 +62,136 @@ test('basic', function (t) {
 
   b.write(b4a.from('hello'))
   b.write(b4a.from('world!'))
-  b.end()
 })
 
-test('write message length, but delay partial message content', function (t) {
+test('partial message length', function (t) {
+  t.plan(8)
+
+  const [a, b] = create()
+
+  a.rawStream.once('data', function (raw) {
+    t.alike(raw, b4a.from([11, 0]), 'a first raw data')
+
+    a.rawStream.once('data', function (raw) {
+      t.alike(raw, b4a.from([0, 0]), 'a second raw data')
+
+      a.rawStream.once('data', function (raw) {
+        t.alike(raw, b4a.from('hello world'), 'a third raw data')
+
+        a.rawStream.once('data', function () {
+          t.fail('a should not receive more raw data')
+        })
+      })
+    })
+  })
+
+  b.rawStream.on('data', function () {
+    t.fail('b should not receive raw data')
+  })
+
+  a.once('data', function (data) {
+    t.alike(data, b4a.from('hello world'), 'a first message')
+
+    a.once('data', function () {
+      t.fail('a should not receive more messages')
+    })
+
+    a.end()
+  })
+
+  a.on('end', function () {
+    t.pass('a end')
+    // a.end()
+  })
+
+  a.on('close', function () {
+    t.pass('a closed')
+  })
+
+  b.once('data', function () {
+    t.fail('b should not receive data')
+  })
+
+  b.on('end', function () {
+    t.pass('b end')
+    b.end()
+  })
+
+  b.on('close', function () {
+    t.pass('b closed')
+  })
+
+  const message = frame(b, b4a.from('hello world'))
+  b.rawStream.write(message.slice(0, 2))
+  setTimeout(() => b.rawStream.write(message.slice(2, 4)), 100)
+  setTimeout(() => b.rawStream.write(message.slice(4)), 200)
+})
+
+test('delay message content', function (t) {
   t.plan(7)
 
   const [a, b] = create()
 
   a.rawStream.once('data', function (raw) {
-    t.alike(raw, b4a.concat([b4a.from([11,0,0,0]), b4a.from('he')]), 'a first raw data')
+    t.alike(raw, b4a.from([11, 0, 0, 0]), 'a first raw data')
+
+    a.rawStream.once('data', function (raw) {
+      t.alike(raw, b4a.from('hello world'), 'a second raw data')
+
+      a.rawStream.once('data', function () {
+        t.fail('a should not receive more raw data')
+      })
+    })
+  })
+
+  b.rawStream.on('data', function () {
+    t.fail('b should not receive raw data')
+  })
+
+  a.once('data', function (data) {
+    t.alike(data, b4a.from('hello world'), 'a first message')
+
+    a.once('data', function () {
+      t.fail('a should not receive more messages')
+    })
+
+    a.end()
+  })
+
+  a.on('end', function () {
+    t.pass('a end')
+    // a.end()
+  })
+
+  a.on('close', function () {
+    t.pass('a closed')
+  })
+
+  b.once('data', function () {
+    t.fail('b should not receive data')
+  })
+
+  b.on('end', function () {
+    t.pass('b end')
+    b.end()
+  })
+
+  b.on('close', function () {
+    t.pass('b closed')
+  })
+
+  const message = frame(b, b4a.from('hello world'))
+  b.rawStream.write(message.slice(0, 4))
+  setTimeout(() => b.rawStream.write(message.slice(4)), 100)
+})
+
+test('delay partial message content', function (t) {
+  t.plan(7)
+
+  const [a, b] = create()
+
+  a.rawStream.once('data', function (raw) {
+    t.alike(raw, b4a.concat([b4a.from([11, 0, 0, 0]), b4a.from('he')]), 'a first raw data')
 
     a.rawStream.once('data', function (raw) {
       t.alike(raw, b4a.from('llo world'), 'a second raw data')
@@ -123,6 +243,189 @@ test('write message length, but delay partial message content', function (t) {
   setTimeout(() => b.rawStream.write(message.slice(6)), 100)
 })
 
+test('several partial message content', function (t) {
+  t.plan(8)
+
+  const [a, b] = create()
+
+  a.rawStream.once('data', function (raw) {
+    t.alike(raw, b4a.from([11, 0, 0, 0]), 'a first raw data')
+
+    a.rawStream.once('data', function (raw) {
+      t.alike(raw, b4a.from('he'), 'a second raw data')
+
+      a.rawStream.once('data', function (raw) {
+        t.alike(raw, b4a.from('llo world'), 'a third raw data')
+
+        a.rawStream.once('data', function () {
+          t.fail('a should not receive more raw data')
+        })
+      })
+    })
+  })
+
+  b.rawStream.on('data', function () {
+    t.fail('b should not receive raw data')
+  })
+
+  a.once('data', function (data) {
+    t.alike(data, b4a.from('hello world'), 'a first message')
+
+    a.once('data', function () {
+      t.fail('a should not receive more messages')
+    })
+
+    a.end()
+  })
+
+  a.on('end', function () {
+    t.pass('a end')
+    // a.end()
+  })
+
+  a.on('close', function () {
+    t.pass('a closed')
+  })
+
+  b.once('data', function () {
+    t.fail('b should not receive data')
+  })
+
+  b.on('end', function () {
+    t.pass('b end')
+    b.end()
+  })
+
+  b.on('close', function () {
+    t.pass('b closed')
+  })
+
+  const message = frame(b, b4a.from('hello world'))
+  b.rawStream.write(message.slice(0, 4))
+  setTimeout(() => b.rawStream.write(message.slice(4, 6)), 100)
+  setTimeout(() => b.rawStream.write(message.slice(6)), 200)
+})
+
+test('big message', function (t) {
+  t.plan(6)
+
+  const [a, b] = create()
+
+  const bigMessageLength = 2 * 1024 * 1024 // => 2097152
+  const bigMessage = b4a.alloc(bigMessageLength).fill('abcd')
+
+  a.rawStream.once('data', function (raw) {
+    /* const frameLength = readUInt32LE(raw.slice(0, 4))
+    t.is(frameLength, bigMessageLength, 'frame message length')
+    t.is(raw.length - 4, bigMessageLength, 'first raw data message length') */
+
+    t.alike(raw, b4a.concat([b4a.from([0, 0, 32, 0]), bigMessage]), 'a first raw data')
+
+    a.rawStream.once('data', function () {
+      t.fail('a should not receive more raw data')
+    })
+  })
+
+  b.rawStream.on('data', function () {
+    t.fail('b should not receive raw data')
+  })
+
+  a.once('data', function (data) {
+    t.alike(data, bigMessage, 'a first message')
+
+    a.once('data', function () {
+      t.fail('a should not receive more messages')
+    })
+
+    a.end()
+  })
+
+  a.on('end', function () {
+    t.pass('a end')
+    // a.end()
+  })
+
+  a.on('close', function () {
+    t.pass('a closed')
+  })
+
+  b.once('data', function () {
+    t.fail('b should not receive data')
+  })
+
+  b.on('end', function () {
+    t.pass('b end')
+    b.end()
+  })
+
+  b.on('close', function () {
+    t.pass('b closed')
+  })
+
+  b.write(bigMessage)
+})
+
+test('destroy', function (t) {
+  t.plan(2)
+
+  const [a, b] = create()
+
+  a.on('close', function () {
+    t.pass('a closed')
+  })
+
+  b.on('close', function () {
+    t.pass('b closed')
+  })
+
+  a.destroy()
+  b.destroy() // + should not be needed?
+})
+
+test('write a string', function (t) {
+  t.plan(6)
+
+  const [a, b] = create()
+
+  a.rawStream.on('data', function (raw) {
+    t.alike(raw, b4a.concat([b4a.from([5, 0, 0, 0]), b4a.from('hello')]), 'a first raw data')
+  })
+
+  b.rawStream.on('data', function () {
+    t.fail('b should not receive raw data')
+  })
+
+  a.on('data', function (data) {
+    t.alike(data, b4a.from('hello'), 'a first message')
+
+    a.end()
+  })
+
+  a.on('end', function () {
+    t.pass('a end')
+    // a.end()
+  })
+
+  a.on('close', function () {
+    t.pass('a closed')
+  })
+
+  b.once('data', function () {
+    t.fail('b should not receive data')
+  })
+
+  b.on('end', function () {
+    t.pass('b end')
+    b.end()
+  })
+
+  b.on('close', function () {
+    t.pass('b closed')
+  })
+
+  b.write('hello')
+})
+
 function frame (stream, data) {
   const wrap = stream._frame(data.byteLength)
   wrap.set(data, stream.frameBytes)
@@ -138,6 +441,13 @@ function create () {
   return [a, b]
 }
 
-function sleep (ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
+/* function readUInt32LE (buf) {
+  return buf[0] + (buf[1] << 8) + (buf[2] << 16) + (buf[3] << 24)
 }
+
+function writeUInt32LE (buf, value) {
+  buf[0] = value
+  buf[1] = value >>> 8
+  buf[2] = value >>> 16
+  buf[3] = value >>> 24
+} */
