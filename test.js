11490bb014f4b5d408fe8ce246926862b9eb1172
Lucas Barrena <lucasbarrena98@gmail.com>
2022-12-06 13:13:16 -0300

    Update test.js

--- test.js
+++ test.js
@@ -63,7 +63,7 @@ test('basic', function (t) {
   b.end()
 })
 
-test.skip('write message length, but delay message content', function (t) {
+test.solo('write message length, but delay partial message content', function (t) {
   t.plan(8)
 
   const [a, b] = create()
@@ -72,7 +72,7 @@ test.skip('write message length, but delay message content', function (t) {
     t.alike(raw, b4a.concat([b4a.from([11,0,0,0]), b4a.from('he')]), 'a first raw data')
 
     a.rawStream.once('data', function (raw) {
-      t.alike(raw, b4a.concat([b4a.from([6,0,0,0]), b4a.from('world!')]), 'a first raw data')
+      t.alike(raw, b4a.from('llo world'), 'a second raw data')
 
       a.rawStream.once('data', function () {
         t.fail()
@@ -85,7 +85,14 @@ test.skip('write message length, but delay message content', function (t) {
   })
 
   a.once('data', function (data) {
+    t.is(data.toString(), 'hello world', 'a first message')
     t.alike(data, b4a.from('hello world'), 'a first message')
+
+    a.once('data', function () {
+      t.fail('a should not receive more messages')
+    })
+
+    // a.end()
   })
 
   a.on('end', function () {
@@ -111,13 +118,24 @@ test.skip('write message length, but delay message content', function (t) {
   })
 
   const message = frame(b, b4a.from('hello world'))
-  b.rawStream.write(message.slice(0, 6), () => console.log('write cb'))
-  setTimeout(() => b.rawStream.write(message.slice(6)), 3000)
+  const firstHalf = message.slice(0, 6)
+  const secondHalf = message.slice(6)
+  console.log('firstHalf', firstHalf)
+  console.log('secondHalf', secondHalf)
+
+  b.rawStream.write(firstHalf)
+  setTimeout(() => b.rawStream.write(secondHalf), 100)
+  setTimeout(() => b.rawStream.end(), 200)
 })
 
 function frame (stream, data) {
+  console.log('frame', data)
+  console.log('data length', data.byteLength)
   const wrap = stream._frame(data.byteLength)
+  console.log('frame length', wrap.byteLength)
   wrap.set(data, stream.frameBytes)
+  console.log('framed length', wrap.slice(0, 4))
+  console.log('framed message', wrap.slice(4).toString())
   return wrap
 }
 
