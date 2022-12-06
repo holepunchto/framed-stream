db2243262e07f4a73a577eee85350049c580dcbf
Lucas Barrena <lucasbarrena98@gmail.com>
2022-12-06 12:23:51 -0300

    using duplex-through

--- test.js
+++ test.js
@@ -1,51 +1,61 @@
 const test = require('brittle')
+const path = require('path')
+const fs = require('fs')
+const fsp = require('fs/promises')
 const FramedStream = require('./index.js')
-const { PassThrough } = require('streamx')
+// const net = require('net')
+const duplexThrough = require('duplex-through')
 const b4a = require('b4a')
 
 test('basic', async function (t) {
-  t.plan(6)
+  t.plan(8)
 
-  const [a, b, pass] = create()
+  const [a, b] = create(t)
 
-  pass.once('data', function (raw) {
-    t.alike(raw, b4a.concat([b4a.from([5,0,0,0]), b4a.from('hello')]))
+  a.stream.once('data', function (raw) {
+    t.alike(raw, b4a.concat([b4a.from([5,0,0,0]), b4a.from('hello')]), 'a first raw data')
+    
+    a.stream.once('data', function (raw) {
+      t.alike(raw, b4a.concat([b4a.from([6,0,0,0]), b4a.from('world!')]), 'a first raw data')
 
-    pass.once('data', function (raw) {
-      t.alike(raw, b4a.concat([b4a.from([6,0,0,0]), b4a.from('world!')]))
-
-      pass.once('data', function () {
+      a.stream.once('data', function () {
         t.fail()
       })
     })
   })
 
+  b.stream.on('data', function () {
+    t.fail('b should not receive raw data')
+  })
+
   a.once('data', function (data) {
-    t.alike(data, b4a.from('hello'))
+    t.alike(data, b4a.from('hello'), 'a first message')
 
     a.once('data', function (data) {
-      t.alike(data, b4a.from('world!'))
+      t.alike(data, b4a.from('world!'), 'a second message')
     })
   })
 
   a.on('end', function () {
-    console.log('a on end')
     a.end()
-    t.pass()
+    t.pass('a end')
   })
 
   a.on('close', function () {
-    console.log('a closed')
+    t.pass('a closed')
+  })
+
+  b.once('data', function () {
+    t.fail('b should not receive data')
   })
 
   b.on('end', function () {
-    console.log('b on end')
+    t.pass('b end')
     b.end()
-    t.pass()
   })
 
   b.on('close', function () {
-    console.log('b closed')
+    t.pass('b closed')
   })
 
   b.write(b4a.from('hello'))
@@ -53,37 +63,59 @@ test('basic', async function (t) {
   b.end()
 })
 
-/* test('basic', async function (t) {
-  return
-  const pass = new PassThrough()
+function create () {
+  const pair = duplexThrough()
 
-  pass.on('data', function (raw) {
-    console.log('raw', raw)
+  const a = new FramedStream(pair[0], { __name: 'a' })
+  const b = new FramedStream(pair[1], { __name: 'b' })
+
+  return [a, b]
+}
+
+/* async function create (t) {
+  const server = net.createServer({ allowHalfOpen: true })
+  const onconnection = new Promise(resolve => server.once('connection', resolve))
+  server.listen(0)
+  await waitForServer(server)
+
+  t.teardown(() => {
+    server.close()
+    return new Promise(resolve => server.once('close', resolve))
   })
 
-  const f = new FramedStream(pass, { bits: 24 })
+  const local = net.connect(server.address().port, server.address().address, { allowHalfOpen: true })
+  const remote = await onconnection
 
-  f.on('data', (data) => console.log(data.toString()))
+  local.setNoDelay()
+  remote.setNoDelay()
 
-  // pass.write(b4a.from([2]))
-  // pass.write(b4a.from([0]))
-  // pass.write(b4a.from([0]))
-  // pass.write(b4a.from([0]))
-  // pass.write(b4a.from('hi'))
-  // pass.write(b4a.concat([b4a.from([2,0,0,0]), b4a.from('ho')]))
+  server.once('connection', function () {
+    t.fail('Socket already existed (another socket connected)')
+  })
 
-  const f2 = new FramedStream(pass, { bits: 24 })
-  // const
+  const a = new FramedStream(local, { __name: 'a' })
+  const b = new FramedStream(remote, { __name: 'b' })
 
-  f2.write(Buffer.from('hello'))
-  f2.write(Buffer.from('world'))
-
-  t.ok(true)
-}) */
-
-function create () {
-  const pass = new PassThrough()
-  const a = new FramedStream(pass, { __name: 'a' })
-  const b = new FramedStream(pass, { __name: 'b' })
-  return [a, b, pass]
+  return [local, remote, a, b]
 }
+
+function waitForServer (server) {
+  return new Promise((resolve, reject) => {
+    server.on('listening', done)
+    server.on('error', done)
+    if (server.listening) done()
+
+    function done (error) {
+      server.removeListener('listening', done)
+      server.removeListener('error', done)
+      error ? reject(error) : resolve()
+    }
+  })
+}
+
+function createTmpDir (t) {
+  const tmpdir = path.join(os.tmpdir(), 'localdrive-test-')
+  const dir = fs.mkdtempSync(tmpdir)
+  t.teardown(() => fsp.rm(dir, { recursive: true }))
+  return dir
+} */
