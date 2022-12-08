7970dae63ad28c82bcae0026582f21a4ca9d11b6
Lucas Barrena <lucasbarrena98@gmail.com>
2022-12-08 14:27:10 -0300

    Revert "chunk.length -> chunk.byteLength"
    This reverts commit 7cad47a85deb7d081710e53de2fde14b442af45a.

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ module.exports = class FramedStream extends Duplex {
       this._message.set(chunk, this._message.byteLength - this._missingBytes)
 
       if (read > data.byteLength) {
-        this._missingBytes -= chunk.byteLength
+        this._missingBytes -= chunk.length
         return
       }
 
