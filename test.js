91a6ddc45c0800ea57b3e5affebea51266db292a
Lucas Barrena <lucasbarrena98@gmail.com>
2022-12-08 17:40:31 -0300

    add test: forward errors when one side is destroyed

--- test.js
+++ test.js
@@ -778,6 +778,27 @@ test('forward errors when both sides are destroyed', async function (t) {
   b.on('close', () => t.pass('b closed'))
 })
 
+test('forward errors when one side is destroyed', async function (t) {
+  t.plan(8)
+
+  const [a, b] = await create()
+
+  const errorA = new Error('error-a')
+  a.destroy(errorA)
+
+  a.rawStream.on('error', (err) => t.is(err, errorA, 'a rawStream: ' + err.message))
+  b.rawStream.on('error', (err) => t.is(err.message, 'Pair was destroyed', 'b rawStream: ' + err.message))
+
+  a.on('error', (err) => t.is(err, errorA, 'a error: ' + err.message))
+  b.on('error', (err) => t.is(err.message, 'Pair was destroyed', 'b error: ' + err.message))
+
+  a.rawStream.on('close', () => t.pass('a rawStream closed'))
+  b.rawStream.on('close', () => t.pass('b rawStream closed'))
+
+  a.on('close', () => t.pass('a closed'))
+  b.on('close', () => t.pass('b closed'))
+})
+
 function frame (stream, data) {
   let len = data.byteLength
   const wrap = b4a.allocUnsafe(len + stream.frameBytes)
